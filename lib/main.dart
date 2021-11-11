cfab598bf859ce6bd639a96cd6a7f2fb84ca1882
Sergey Anisov <sanisov@tomsksoft.com>
2021-11-11 18:09:25 +0700

    переводы

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/custom_theme.dart';
 import 'package:flutter_app/screens/main_screen/main_screen.dart';
 import 'package:flutter_app/services/app_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,50 +24,14 @@ class MyApp extends StatelessWidget {
           return BlocProvider(
             create: (_) => CustomThemeCubit(initTheme: initTheme),
             child: const MaterialApp(
-              title: 'Flutter Demo',
-              home: MyHomePage(title: 'Flutter Demo Home Page'),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              title: "ТЕСТ",
+              home:  MainScreen(),
             ),
           );
         }
       },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MainScreen();
   }
 }
